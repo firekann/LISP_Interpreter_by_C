9ade37b1a320b378f4473b56f21554ac8b092c13
Homin Lee <leehomin118@gmail.com>
2020-12-04 05:44:30 +0900

    중첩된 구문 적용

--- LISP_Interpreter/LISP_Interpreter/OP~NTH.c
+++ LISP_Interpreter/LISP_Interpreter/OP~NTH.c
@@ -2,6 +2,7 @@
 #include "OP~NTH.h"
 #include "type.h"
 #include "lexer.h"
+#include "util.h"
 #include <stdio.h>
 
 T_OBJ fn_add() {
@@ -21,6 +22,7 @@ T_OBJ fn_add() {
 	float sum = 0;
 	int type = cur_node->value.type;
 	while (cur_node->value.type != RIGHT_PAREN) {
+		bool flag = true;
 		if (cur_node->value.type == INT) {
 			sum += (float)cur_node->value.t_int;
 		}
@@ -29,8 +31,22 @@ T_OBJ fn_add() {
 			sum += (float)cur_node->value.t_float;
 		}
 		else if (cur_node->value.type == LEFT_PAREN) {	// 괄호가 나올 경우 처리함
-			left_paren_Count++;
-			/////do something
+			flag = false;
+			T_OBJ tmp = call_fn();
+			if (tmp.type == INT) {
+				if (cnt == 0) {
+					type = INT;
+				}
+				sum += (float)tmp.t_int;
+			}
+			else if (tmp.type == FLOAT) {
+				type = FLOAT;
+				sum += (float)tmp.t_float;
+			}
+			else {
+				printf("ERROR : TYPE ERROR FOR ADD\n");
+				return;
+			}
 		}
 		else if (cnt == 0 && cur_node->value.type == IDENT) {	//add의 첫번째 인자가 IDENT일 경우 그 IDENT의 값을 바꿔줌
 			is_ident = true;
@@ -44,21 +60,23 @@ T_OBJ fn_add() {
 				sum = (float)tmp.t_float;
 			}
 			else {
-				printf("ERROR : SYNTAX ERROR FOR ADD\n");
+				printf("ERROR : TYPE ERROR FOR ADD\n");
 				return;
 			}
 			key = (char*)malloc(sizeof(char)*cur_node->value.lexed_len + 1);
 			strcpy(key, cur_node->value.t_string);
 		}
 		else {
-			printf("ERROR : NOT ADD TYPE\n");
+			printf("ERROR : TYPE ERROR FOR ADD\n");
 			return;
 		}
 		if (cur_node->next == NULL) {
 			printf("ERROR : SYNTAX ERROR FOR ADD\n");
 			return;
 		}
-		cur_node = cur_node->next;
+		if (flag) {
+			cur_node = cur_node->next;
+		}
 		cnt++;
 	}
 	if (cnt == 0) {
@@ -107,8 +125,10 @@ T_OBJ fn_sub() {
 	float sum = 0;
 	int type = cur_node->value.type;
 	while (cur_node->value.type != RIGHT_PAREN) {
+		bool flag = true;
 		if (cur_node->value.type == INT) {
 			if (cnt == 0) {	//sub의 경우 처음 초기값이 존재해야함
+				type = INT;
 				sum = (float)cur_node->value.t_int;
 			}
 			else {
@@ -125,10 +145,29 @@ T_OBJ fn_sub() {
 			}
 		}
 		else if (cur_node->value.type == LEFT_PAREN) {	// 괄호가 나올 경우 처리함
-			left_paren_Count++;
-			/////do something
-			if (cnt == 0) {	//sub의 경우 처음 초기값이 존재해야함
-
+			flag = false;
+			T_OBJ tmp = call_fn();
+			if (tmp.type == INT) {
+				if (cnt == 0) {	//sub의 경우 처음 초기값이 존재해야함
+					type = INT;
+					sum = (float)tmp.t_int;
+				}
+				else {
+					sum -= (float)tmp.t_int;
+				}
+			}
+			else if (tmp.type == FLOAT) {
+				type = FLOAT;
+				if (cnt == 0) {	//sub의 경우 처음 초기값이 존재해야함
+					sum = (float)tmp.t_float;
+				}
+				else {
+					sum -= (float)tmp.t_float;
+				}
+			}
+			else {
+				printf("ERROR : TYPE ERROR FOR SUB\n");
+				return;
 			}
 		}
 		else if (cnt == 0 && cur_node->value.type == IDENT) {	//sub의 첫번째 인자가 IDENT일 경우 그 IDENT의 값을 바꿔줌
@@ -143,21 +182,23 @@ T_OBJ fn_sub() {
 				sum = (float)tmp.t_float;
 			}
 			else {
-				printf("ERROR : SYNTAX ERROR FOR SUB\n");
+				printf("ERROR : TYPE ERROR FOR SUB\n");
 				return;
 			}
 			key = (char*)malloc(sizeof(char)*cur_node->value.lexed_len + 1);
 			strcpy(key, cur_node->value.t_string);
 		}
 		else {
-			printf("ERROR : NOT SUB TYPE\n");
+			printf("ERROR : TYPE ERROR FOR SUB\n");
 			return;
 		}
 		if (cur_node->next == NULL) {
 			printf("ERROR : SYNTAX ERROR FOR SUB\n");
 			return;
 		}
-		cur_node = cur_node->next;
+		if (flag) {
+			cur_node = cur_node->next;
+		}
 		cnt++;
 	}
 	if (cnt == 0) {
@@ -206,6 +247,7 @@ T_OBJ fn_mul() {
 	float sum = 1;
 	int type = cur_node->value.type;
 	while (cur_node->value.type != RIGHT_PAREN) {
+		bool flag = true;
 		if (cur_node->value.type == INT) {
 			sum *= (float)cur_node->value.t_int;
 		}
@@ -214,8 +256,22 @@ T_OBJ fn_mul() {
 			sum *= (float)cur_node->value.t_float;
 		}
 		else if (cur_node->value.type == LEFT_PAREN) {	// 괄호가 나올 경우 처리함
-			left_paren_Count++;
-			/////do something
+			flag = false;
+			T_OBJ tmp = call_fn();
+			if (tmp.type == INT) {
+				if (cnt == 0) {
+					type = INT;
+				}
+				sum *= (float)tmp.t_int;
+			}
+			else if (tmp.type == FLOAT) {
+				type = FLOAT;
+				sum *= (float)tmp.t_float;
+			}
+			else {
+				printf("ERROR : TYPE ERROR FOR MUL\n");
+				return;
+			}
 		}
 		else if (cnt == 0 && cur_node->value.type == IDENT) {	//mul의 첫번째 인자가 IDENT일 경우 그 IDENT의 값을 바꿔줌
 			is_ident = true;
@@ -229,21 +285,23 @@ T_OBJ fn_mul() {
 				sum = (float)tmp.t_float;
 			}
 			else {
-				printf("ERROR : SYNTAX ERROR FOR MUL\n");
+				printf("ERROR : TYPE ERROR FOR MUL\n");
 				return;
 			}
 			key = (char*)malloc(sizeof(char)*cur_node->value.lexed_len + 1);
 			strcpy(key, cur_node->value.t_string);
 		}
 		else {
-			printf("ERROR : NOT MUL TYPE\n");
+			printf("ERROR : TYPE ERROR FOR MUL\n");
 			return;
 		}
 		if (cur_node->next == NULL) {
 			printf("ERROR : SYNTAX ERROR FOR MUL\n");
 			return;
 		}
-		cur_node = cur_node->next;
+		if (flag) {
+			cur_node = cur_node->next;
+		}
 		cnt++;
 	}
 	if (cnt == 0) {
@@ -292,6 +350,7 @@ T_OBJ fn_div() {
 	float sum = 0;
 	int type = cur_node->value.type;
 	while (cur_node->value.type != RIGHT_PAREN) {
+		bool flag = true;
 		if (cur_node->value.type == INT) {
 			if (cnt == 0) {	//div의 경우 처음 초기값이 존재해야함
 				sum = (float)cur_node->value.t_int;
@@ -310,10 +369,29 @@ T_OBJ fn_div() {
 			}
 		}
 		else if (cur_node->value.type == LEFT_PAREN) {	// 괄호가 나올 경우 처리함
-			left_paren_Count++;
-			/////do something
-			if (cnt == 0) {	//div의 경우 처음 초기값이 존재해야함
-
+			flag = false;
+			T_OBJ tmp = call_fn();
+			if (tmp.type == INT) {
+				if (cnt == 0) {	//div의 경우 처음 초기값이 존재해야함
+					type = INT;
+					sum = (float)tmp.t_int;
+				}
+				else {
+					sum /= (float)tmp.t_int;
+				}
+			}
+			else if (tmp.type == FLOAT) {
+				type = FLOAT;
+				if (cnt == 0) {	//div의 경우 처음 초기값이 존재해야함
+					sum = (float)tmp.t_float;
+				}
+				else {
+					sum /= (float)tmp.t_float;
+				}
+			}
+			else {
+				printf("ERROR : TYPE ERROR FOR DIV\n");
+				return;
 			}
 		}
 		else if (cnt == 0 && cur_node->value.type == IDENT) {	//div의 첫번째 인자가 IDENT일 경우 그 IDENT의 값을 바꿔줌
@@ -328,21 +406,23 @@ T_OBJ fn_div() {
 				sum = (float)tmp.t_float;
 			}
 			else {
-				printf("ERROR : SYNTAX ERROR FOR DIV\n");
+				printf("ERROR : TYPE ERROR FOR DIV\n");
 				return;
 			}
 			key = (char*)malloc(sizeof(char)*cur_node->value.lexed_len + 1);
 			strcpy(key, cur_node->value.t_string);
 		}
 		else {
-			printf("ERROR : NOT DIV TYPE\n");
+			printf("ERROR : TYPE ERROR FOR DIV\n");
 			return;
 		}
 		if (cur_node->next == NULL) {
 			printf("ERROR : SYNTAX ERROR FOR DIV\n");
 			return;
 		}
-		cur_node = cur_node->next;
+		if (flag) {
+			cur_node = cur_node->next;
+		}
 		cnt++;
 	}
 	if (cnt == 0) {
@@ -397,45 +477,63 @@ T_OBJ fn_setq() {
 		return;
 	}
 
-	if (cur_node->value.type == LEFT_PAREN) {	// 괄호가 나올 경우 처리함
-		left_paren_Count++;
-		/////do something
+	T_OBJ res;
+	if (cur_node->value.type == SQUOTE) {	//이 경우는 리스트 입력을 받음
+
 	}
-	else if (cur_node->value.type == IDENT) {	//a=b의 경우 b의 값을 a에 대입 하는 느낌
-		T_OBJ v = get_dict_obj(dict, cur_node->value.t_string);	//dict에서 먼저 값을 가져옴
-		insert_dict_node(dict, symbol, &v);
-		T_OBJ tmp = cur_node->value;
-		cur_node = cur_node->next;
-		if (cur_node->value.type == RIGHT_PAREN) {
-			right_paren_Count++;
-			cur_node = cur_node->next;
-			return tmp;
+	else if (cur_node->value.type == LEFT_PAREN) {	// 괄호가 나올 경우 처리함
+		T_OBJ tmp = call_fn();	//리턴값이 있는 함수의 경우
+		if (tmp.type == INT || tmp.type == FLOAT || tmp.type == STRING || tmp.type == T_LIST || tmp.type == BOOLEAN || tmp.type == NIL) {
+			insert_dict_node(dict, symbol, &tmp);
+			res = tmp;
 		}
 		else {
-			printf("ERROR : NO RIGHT_PAREN FOR SETQ\n");
+			printf("ERROR : TYPE ERROR FOR SETQ\n");
 			return;
 		}
+	}
+	else if (cur_node->value.type == IDENT) {	//a=b의 경우 b의 값을 a에 대입 하는 느낌
+		T_OBJ tmp = get_dict_obj(dict, cur_node->value.t_string);	//dict에서 먼저 값을 가져옴
+		insert_dict_node(dict, symbol, &tmp);
+		res = tmp;
+		cur_node = cur_node->next;
 	}
 	else {
 		// 정수 실수 문자열의 경우
 		if (cur_node->value.type == INT || cur_node->value.type == FLOAT || cur_node->value.type == STRING) {
 			insert_dict_node(dict, symbol, &(cur_node->value));
-			T_OBJ tmp = cur_node->value;
+			res = cur_node->value;
 			cur_node = cur_node->next;
-			if (cur_node->value.type == RIGHT_PAREN) {
-				right_paren_Count++;
-				cur_node = cur_node->next;
-				return tmp;
-			}
-			else {
-				printf("ERROR : NO RIGHT_PAREN FOR SETQ\n");
-				return;
-			}
 		}
 		else {
-			printf("ERROR : VALUE ERROR\n");
+			printf("ERROR : VALUE ERROR FOR SETQ\n");
 			return;
 		}
 	}
 	free(symbol);
+	if (cur_node->value.type == RIGHT_PAREN) {
+		right_paren_Count++;
+		cur_node = cur_node->next;
+		return res;
+	}
+	else {
+		printf("ERROR : NO RIGHT_PAREN FOR SETQ\n");
+		return;
+	}
+}
+
+T_OBJ fn_list() {
+
+}
+
+T_OBJ fn_car() {
+
+}
+
+T_OBJ fn_cdr() {
+
+}
+
+T_OBJ fn_nth() {
+
 }
