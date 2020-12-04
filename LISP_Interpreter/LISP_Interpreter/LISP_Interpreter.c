b9b3dc0db4ff9cf31da8d96c87c9b52f51c55a99
wan8234 <wan8234@naver.com>
2020-12-04 21:18:33 +0900

    edit main function to show posible  result

--- LISP_Interpreter/LISP_Interpreter/LISP_Interpreter.c
+++ LISP_Interpreter/LISP_Interpreter/LISP_Interpreter.c
@@ -44,7 +44,6 @@ int main()
 {
 	dict = initialize_dict();
 	while (1) {
-		left_paren_Count = right_paren_Count = 0;
 		obj_list = initialize_list();
 
 		printf("Enter the lisp line: ");
@@ -56,10 +55,51 @@ int main()
 		do {
 			lex();
 		} while (nextToken != EOF);
-
-		cur_node = obj_list->head;
-		call_fn();
 		
+		cur_node = obj_list->head;
+		T_OBJ decision = call_fn();
+		if (left_paren_Count != right_paren_Count){
+			printf("NIL\n");
+		}
+		else if (decision.t_bool == true){
+			if (decision.type == INT){
+				printf("%d\n", decision.t_int);
+			}
+			else if (decision.type == FLOAT){
+				printf("%f\n", decision.t_float);
+			}
+			else if (decision.type == STRING){
+				printf("%s\n", decision.t_string);
+			}
+			else if (decision.type == BOOLEAN){
+				printf("T\n");
+			}
+			else if (decision.type == T_LIST){
+				int cycle = decision.t_int;
+				T_OBJ *list;
+				T_OBJ *listNext = &decision;
+				printf("( ");
+				for (int i = 0; i < cycle; i++){
+					list = listNext;
+					listNext = listNext->next;
+					list = list->t_list_value;
+					if (list->type == INT){
+						printf("%d ", list->t_int);
+					}
+					else if (list->type == FLOAT){
+						printf("%f ", list->t_float);
+					}
+					else if (list->type == STRING){
+						printf("%s ", list->t_string);
+					}
+				}
+				printf(")\n");
+			}
+		}
+		else{
+			printf("NIL\n");
+		}
+
 		DICT_NODE* dict_node = dict->head;
 		while (dict_node != NULL)
 		{
@@ -74,15 +114,18 @@ int main()
 		}
 
 		/*
-		LIST_NODE* list_node = obj_list->head;
-		while (list_node != NULL)
+		LIST_NODE* node = obj_list->head;
+		while (node != NULL)
 		{
-			printf("Next token is: %d, Next lexeme is %s\n", list_node->value.type, list_node->value.t_string);
-			list_node = list_node->next;
+		printf("Next token is: %d, Next lexeme is %s\n", node->value.type, node->value.t_string);
+		node = node->next;
 		}
 		*/
 
 		ifFlag = 0;
+		left_paren_Count = 0;
+		right_paren_Count = 0;
+
 		free_list(obj_list);
 	}
 	free_dict(dict);
