2d59393af6202a07a5223fa99a6670418b56ed9e
Homin Lee <leehomin118@gmail.com>
2020-12-06 11:03:52 +0900

    IDENT일 경우 출력함

--- LISP_Interpreter/LISP_Interpreter/LISP_Interpreter.c
+++ LISP_Interpreter/LISP_Interpreter/LISP_Interpreter.c
@@ -19,33 +19,8 @@
 /* main driver                            */
 /******************************************/
 
-void print_list(T_OBJ* value) {
-	//printf("list size : %d : ", value->t_int);
-	T_OBJ *list;
-	T_OBJ *listNext = value;
-	printf("(");
-	for (int i = 0; i < value->t_int; i++) {
-		list = listNext;
-		listNext = listNext->next;
-		list = list->t_list_value;
-		if (list->type == INT) {
-			printf("%d", list->t_int);
-		}
-		else if (list->type == FLOAT) {
-			printf("%f", list->t_float);
-		}
-		else if (list->type == STRING) {
-			printf("%s", list->t_string);
-		}
-		else if (list->type == T_LIST) {
-			print_list(list);
-		}
-		if (i + 1 != value->t_int)
-			printf(" ");
-	}
-	printf(")");
-	return;
-}
+void print_obj(T_OBJ*);
+void print_list(T_OBJ*);
 
 int main(int argc, char* argv[])
 {
@@ -72,27 +47,23 @@ int main(int argc, char* argv[])
 
 	cur_node = obj_list->head;
 	while (cur_node->value.type != EOF) {
+		if (cur_node->value.type == IDENT) {
+			if (has_dict_key(dict, cur_node->value.t_string)) {
+				T_OBJ tmp = get_dict_obj(dict, cur_node->value.t_string);
+				print_obj(&tmp);
+			}
+			else {
+				printf("NO IDENT\n");
+			}
+			cur_node = cur_node->next;
+			continue;
+		}
 		T_OBJ decision = call_fn();
 		if (left_paren_Count != right_paren_Count) {
 			printf("NIL\n");
 		}
 		else if (decision.t_bool == true) {
-			if (decision.type == INT) {
-				printf("%d\n", decision.t_int);
-			}
-			else if (decision.type == FLOAT) {
-				printf("%f\n", decision.t_float);
-			}
-			else if (decision.type == STRING) {
-				printf("%s\n", decision.t_string);
-			}
-			else if (decision.type == BOOLEAN) {
-				printf("T\n");
-			}
-			else if (decision.type == T_LIST) {
-				print_list(&decision);
-				printf("\n");
-			}
+			print_obj(&decision);
 		}
 		else if (nextToken == EOF) {
 			printf("EOF\n");
@@ -120,10 +91,56 @@ int main(int argc, char* argv[])
 
 		left_paren_Count = 0;
 		right_paren_Count = 0;
-		printf("\n");
 	}
 	free_list(obj_list);
 	free_dict(dict);
 	fclose(in_fp);
 	return 0;
+}
+
+void print_obj(T_OBJ* value) {
+	if (value->type == INT) {
+		printf("%d\n", value->t_int);
+	}
+	else if (value->type == FLOAT) {
+		printf("%f\n", value->t_float);
+	}
+	else if (value->type == STRING) {
+		printf("%s\n", value->t_string);
+	}
+	else if (value->type == BOOLEAN) {
+		printf("T\n");
+	}
+	else if (value->type == T_LIST) {
+		print_list(value);
+		printf("\n");
+	}
+}
+
+void print_list(T_OBJ* value) {
+	//printf("list size : %d : ", value->t_int);
+	T_OBJ *list;
+	T_OBJ *listNext = value;
+	printf("(");
+	for (int i = 0; i < value->t_int; i++) {
+		list = listNext;
+		listNext = listNext->next;
+		list = list->t_list_value;
+		if (list->type == INT) {
+			printf("%d", list->t_int);
+		}
+		else if (list->type == FLOAT) {
+			printf("%f", list->t_float);
+		}
+		else if (list->type == STRING) {
+			printf("%s", list->t_string);
+		}
+		else if (list->type == T_LIST) {
+			print_list(list);
+		}
+		if (i + 1 != value->t_int)
+			printf(" ");
+	}
+	printf(")");
+	return;
 }
