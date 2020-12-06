6cc15eec250393025a60cccdfecacc73d5e8c0e0
JKTG94 <55914016+JKTG94@users.noreply.github.com>
2020-12-06 23:12:23 +0900

    음.

--- LISP_Interpreter/LISP_Interpreter/CONS~SUBST.c
+++ LISP_Interpreter/LISP_Interpreter/CONS~SUBST.c
@@ -279,81 +279,106 @@ T_OBJ fn_append(){
 //EX) (length 'a 'b) => 2
 // APPEND 하고나서 길이를 계산해준다
 T_OBJ fn_length(){
-    int tmp_count = 0;
 	if (cur_node->value.type == LEFT_PAREN) {	//왼쪽 괄호 확인
 		left_paren_Count++;
-        tmp_count++;
 		cur_node = cur_node->next;
 		cur_node = cur_node->next;
 	}
 	else {
-		printf("ERROR");
-		return;
+		printf("ERROR : NO LEFT_PAREN FOR LIST\n");
+		return return_false();
 	}
-    int cnt = 0;
-    c_LIST* tmp_list = initialize_list();
 
-        while(cur_node->value.type == RIGHT_PAREN){
-            T_OBJ tmp;
-            if(cur_node->value.type == SQUOTE){
-                cur_node = cur_node->next;
-                if(cur_node->value.type == LEFT_PAREN){
-                    LIST_NODE* tmp_node = cur_node->next;
-                    if (tmp_node->value.type == INT || tmp_node->value.type == FLOAT || tmp_node->value.type == STRING || tmp_node->value.type == BOOLEAN) {
-                        //괄호 뒤의 토큰이 함수가 아니면 make_list를 호출해준다.
-                        tmp = fn_make_list();
-                    }
-                    else {
-                        //괄호 뒤의 토큰이 함수라면 call_fn을 통해서 함수를 호출해준다.
-                        tmp = call_fn();
-                    }
-                }
-                else{
-                    tmp = cur_node->value;
-                    cur_node = cur_node->next;
-                }
-            }
-            else if (cur_node->value.type == IDENT) {
-                tmp = get_dict_obj(dict, cur_node->value.t_string);
-                cur_node = cur_node->next;
-            }
-            else {
-                printf("ERROR : TYPE ERROR FOR LIST\n");
-                free_list(tmp_list);
-                return;
-            }
-
-            if (tmp.type == IDENT) {	//IDENT의 경우 STRING으로 취급해서 처리
-                tmp.type = STRING;
-                insert_list_node(tmp_list, &tmp);
-            }
-            else if (tmp.type == INT || tmp.type == FLOAT || tmp.type == STRING || tmp.type == BOOLEAN || tmp.type == T_LIST) {	//이 경우는 그냥 할당
-                insert_list_node(tmp_list, &tmp);
-            }
-            else {
-                printf("ERROR : TYPE ERROR FOR LIST\n");
-                free_list(tmp_list);
-                return;
-            }
-            cnt++;
-    	}
-
+	int cnt = 0;
+	c_LIST* tmp_list = initialize_list();	//리스트를 생성하기 위한 임시 리스트
+	while (cur_node->value.type != RIGHT_PAREN) {	//오른쪽 괄호가 나오기 전까지 반복
+		T_OBJ tmp;
+		if (cur_node->value.type == SQUOTE) {
+			cur_node = cur_node->next;
+			if (cur_node->value.type == LEFT_PAREN) {	//괄호가 올 경우 처리함
+				tmp = fn_make_list();
+			}
+			else {	//괄호가 없을 경우 현재 노드의 value를 tmp에 할당
+				tmp = cur_node->value;
+				cur_node = cur_node->next;
+				if (!(tmp.type == INT || tmp.type == FLOAT || tmp.type == STRING || tmp.type == BOOLEAN || tmp.type == T_LIST)) {
+					tmp.type = STRING;
+				}
+			}
+		}
+		else if (cur_node->value.type == LEFT_PAREN) {	//이 경우 함수를 호출해서 처리함.
+			tmp = call_fn();
+		}
+		else if (cur_node->value.type == INT || cur_node->value.type == FLOAT || cur_node->value.type == STRING || cur_node->value.type == BOOLEAN) {
+			tmp = cur_node->value;
+			cur_node = cur_node->next;
+		}
+		else if (cur_node->value.type == IDENT) {
+			tmp = get_dict_obj(dict, cur_node->value.t_string);
+			cur_node = cur_node->next;
+		}
+		else {
+			printf("ERROR : TYPE ERROR FOR LIST\n");
+			free_list(tmp_list);
+			return return_false();
+		}
+		if (tmp.type == IDENT) {	//IDENT의 경우 STRING으로 취급해서 처리
+			tmp.type = STRING;
+			insert_list_node(tmp_list, &tmp);
+		}
+		else if (tmp.type == INT || tmp.type == FLOAT || tmp.type == STRING || tmp.type == BOOLEAN || tmp.type == T_LIST) {	//이 경우는 그냥 할당
+			insert_list_node(tmp_list, &tmp);
+		}
+		else {
+			printf("ERROR : TYPE ERROR FOR LIST\n");
+			free_list(tmp_list);
+			return return_false();
+		}
+		cnt++;
+	}
+	//printf("list size : %d , %d\n", tmp_list->list_size, cnt);	//디버깅용
+	T_OBJ head;
+	T_OBJ* pre_obj = &head;
+	head.type = T_LIST;
+	head.t_int = cnt;
+	head.next = head.t_list_value = NULL;
+	head.t_bool = true;
+	if (cnt == 0) {	//인자가 0개면 길이가 0인 리스트를 반환한다.
+		return head;
+	}
+	//아니라면 임시로 만든 리스트의 값을 이용해서 리스트를 생성한다.
+	LIST_NODE* tmp_node = tmp_list->head;
 	T_OBJ result;
 	result.type = INT;
 	result.t_int = cnt;
+	while (tmp_node != NULL) {
+		if (head.t_list_value == NULL) {
+			head.t_list_value = &(tmp_node->value);
+		}
+		else {
+			T_OBJ* tmp = malloc(sizeof(T_OBJ));
+			tmp->type = T_LIST;
+			tmp->t_int = cnt;
+			tmp->t_list_value = &(tmp_node->value);
+			tmp->t_bool = true;
+			tmp->next = NULL;
+			pre_obj->next = tmp;
+			pre_obj = tmp;
+		}
+		tmp_node = tmp_node->next;
+	}
 	if (cur_node->value.type == RIGHT_PAREN) {
 		right_paren_Count++;
 		cur_node = cur_node->next;
-        return result;
 		free(tmp_list);
+		return result;
 	}
 	else {
 		printf("ERROR : NO RIGHT_PAREN FOR LIST\n");
 		free_list(tmp_list);
-		return;
+		return return_false();
 	}
 }
-
 
 //MEMBER
 /* EX)
